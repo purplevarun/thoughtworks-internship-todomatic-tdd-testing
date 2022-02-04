1a6fbdb2f9b40fbb6c84675fcd0be6269551e49f
purplevarun <purplevarun@gmail.com>
2022-02-05 02:44:55 +0530

    testarea clearance after btn click test passed!

--- src/tests/NoteArea.test.jsx
+++ src/tests/NoteArea.test.jsx
@@ -1,6 +1,12 @@
 import "@testing-library/jest-dom/extend-expect";
 import { fireEvent, render } from "@testing-library/react";
-import NoteArea from "./../components/NoteArea";
+import { useState } from "react";
+import NoteArea from "../components/NoteArea";
+
+var allNotes = [];
+const setAllNotes = (newNotes) => {
+	allNotes = newNotes;
+};
 
 describe("text area tests", () => {
 	it("should be empty initially", () => {
@@ -15,5 +21,14 @@ describe("text area tests", () => {
 		fireEvent.change(textArea, { target: { value: newNote } }); // using firevent to put text inside the textarea box
 		expect(textArea.value).toBe(newNote);
 	});
-	it("should clear textarea after submit button has been clicked", () => {});
+	it("should clear textarea after submit button has been clicked", () => {
+		const newNote = "hello world";
+		const noteArea = render(
+			<NoteArea allNotes={allNotes} setAllNotes={setAllNotes} />
+		);
+		const textArea = noteArea.getByTestId("notebox");
+		fireEvent.change(textArea, { target: { value: newNote } }); // using firevent to put text inside the textarea box
+		fireEvent.click(noteArea.getByTestId("submitbtn"));
+		expect(textArea.value).toBe("");
+	});
 });
