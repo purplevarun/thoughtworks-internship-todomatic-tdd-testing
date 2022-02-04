fdeac206db0f85b429de6044ae207f6983f80664
purplevarun <purplevarun@gmail.com>
2022-02-05 02:32:17 +0530

    2 tc passed

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -6,13 +6,14 @@ describe("text area tests", () => {
 	it("should be empty initially", () => {
 		const noteArea = render(<NoteArea />);
 		const textArea = noteArea.getByTestId("notebox");
-		expect(textArea).toHaveTextContent("");
+		expect(textArea.value).toBe("");
 	});
 	it("should change it's value on text input", () => {
 		const newNote = "hello world";
 		const noteArea = render(<NoteArea />);
 		const textArea = noteArea.getByTestId("notebox");
 		fireEvent.change(textArea, { target: { value: newNote } }); // using firevent to put text inside the textarea box
+		expect(textArea.value).toBe(newNote);
 	});
 	it("should clear textarea after submit button has been clicked", () => {});
 });
