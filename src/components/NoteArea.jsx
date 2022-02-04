0e7cbd43bb13c8f002277035117923f60a07a50c
purplevarun <purplevarun@gmail.com>
2022-02-04 12:51:18 +0530

    divided page into two flexboxed

--- src/components/NoteArea.jsx
+++ src/components/NoteArea.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import "./NoteArea.css";
 const NoteArea = () => {
-	return <div className="note-area"></div>;
+	return (
+		<div className="note-area">
+			<div className="">
+				<textarea name="" id="" cols="40" rows="10"></textarea>
+			</div>
+		</div>
+	);
 };
 
 export default NoteArea;
--- src/components/SavedNotes.jsx
+++ src/components/SavedNotes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./SavedNotes.css";
 const SavedNotes = () => {
 	return <div className="saved-notes"></div>;
 };
